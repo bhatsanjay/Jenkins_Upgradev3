a7aac39d97d2defe1591df7e08c258b22ca9e180
Bhat, Sanjay <ssbtrainlogin@gmail.com>
2020-04-26 23:04:42 +0530

    maven test message 1

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,6 +37,8 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
   public void greetShouldIncludeGreetingMessageAgain() {
     String someone = "World, Thanks you making me Happy Again";
 
